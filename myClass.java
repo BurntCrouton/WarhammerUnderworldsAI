f3d6d228e2ea2a3b1d3d0d4800da713a0284bd74
BurntCrouton <124568751+BurntCrouton@users.noreply.github.com>
2024-05-03 12:47:45 +0200

    Update myClass.java

--- myClass.java
+++ myClass.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class myClass {
     public static void main(String[] args) {
       int numBoardHexesY;
@@ -5,24 +7,29 @@ public class myClass {
       int numSingleBoardHexesY;
       int numSingleBoardHexesX;
       
-      boolean boardLong = false; 
-      boolean boardWide = true; // the different Board layouts possible in Warhammer Underworlds
-      
+      // boolean boardLong = false; 
+      // boolean boardWide = false; // the different Board layouts possible in Warhammer Underworlds
       
       int[] dualLengthBoard = {5, 8, 5, 16};
       int[] dualWidthBoard = {5, 8, 10, 8};
 
-      if(boardLong == true) {
+      Scanner myObj = new Scanner(System.in); 
+      System.out.println("Enter preferred board Format: ");
+      String boardFormat = myObj.nextLine();
+
+      if(boardFormat == "boardLong") {
+        System.out.println("Board Format is: " + boardFormat);
         numBoardHexesY = dualLengthBoard[3];
         numBoardHexesX = dualLengthBoard[2];
         numSingleBoardHexesY = dualLengthBoard[1];
         numSingleBoardHexesX = dualLengthBoard[0];
-      } else if(boardWide == true) {
+      } else if(boardFormat == "boardWide") {
         numBoardHexesY = dualWidthBoard[3];
         numBoardHexesX = dualWidthBoard[2];
         numSingleBoardHexesY = dualWidthBoard[1];
         numSingleBoardHexesX = dualWidthBoard[0];
 
+        System.out.println("Board Format is: " + boardFormat); 
         System.out.println(numBoardHexesY);
         System.out.println(numBoardHexesX);
         System.out.println(numSingleBoardHexesY);
