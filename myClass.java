7e53fd8f35c059e40d68118f54bac706768fdab1
BurntCrouton <124568751+BurntCrouton@users.noreply.github.com>
2024-05-03 13:09:16 +0200

    Update myClass.java

--- myClass.java
+++ myClass.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 
 public class myClass {
     public static void main(String[] args) {
@@ -6,6 +8,7 @@ public class myClass {
       int numBoardHexesX;
       int numSingleBoardHexesY;
       int numSingleBoardHexesX;
+      String globalboardFormat;
       
       // boolean boardLong = false; 
       // boolean boardWide = false; // the different Board layouts possible in Warhammer Underworlds
@@ -15,31 +18,39 @@ public class myClass {
 
       Scanner myObj = new Scanner(System.in); 
       System.out.println("Enter preferred board Format: ");
-      String boardFormat = myObj.nextLine();
       
-      if(boardFormat == "boardLong") {
+      try {
+          TimeUnit.SECONDS.sleep(5);
+          String boardFormat = myObj.nextLine();
+          globalboardFormat = boardFormat;
+      } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+      }
+      
+      
+      if(globalboardFormat == "boardLong") {
         numBoardHexesY = dualLengthBoard[3];
         numBoardHexesX = dualLengthBoard[2];
         numSingleBoardHexesY = dualLengthBoard[1];
         numSingleBoardHexesX = dualLengthBoard[0];
 
-        System.out.println("Board Format is: " + boardFormat); 
+        System.out.println("Board Format is: " + globalboardFormat); 
         System.out.println(numBoardHexesY);
         System.out.println(numBoardHexesX);
         System.out.println(numSingleBoardHexesY);
         System.out.println(numSingleBoardHexesX);
-      } else if(boardFormat == "boardWide") {
+      } else if(globalboardFormat == "boardWide") {
         numBoardHexesY = dualWidthBoard[3];
         numBoardHexesX = dualWidthBoard[2];
         numSingleBoardHexesY = dualWidthBoard[1];
         numSingleBoardHexesX = dualWidthBoard[0];
 
-        System.out.println("Board Format is: " + boardFormat); 
+        System.out.println("Board Format is: " + globalboardFormat); 
         System.out.println(numBoardHexesY);
         System.out.println(numBoardHexesX);
         System.out.println(numSingleBoardHexesY);
         System.out.println(numSingleBoardHexesX);
-      } else if(boardFormat != "boardWide" && boardFormat != "boardLong") {
+      } else if(globalboardFormat != "boardWide" && globalboardFormat != "boardLong") {
         System.out.println("404 Board Format not found");
       }
     }
